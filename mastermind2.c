53f92ee6201c6637475fbce9f32dc07468c2088a
asamuels <american_cyko@yahoo.com>
2015-11-08 18:20:28 -0800

    Various tweaks to mastermind

--- mastermind2.c
+++ mastermind2.c
@@ -4,59 +4,20 @@
 #include <time.h>
 
 #define MAX 6
+#define FOUR 4
 
 void userInput(char* input);
-void compInput(char guess[],int red, int white, int* count,int* onePlace,int* twoPlace,int* threePlace,int* fourPlace);
-int compGame();
 int game();
 int compare(int red, int white,int target[], char guess[]);
 
 int main(void)
 {
 	srand(time(NULL));
-	//compGame();
 
 	game();
-
+	//compGame();
 }
 
-int compGame()
-{
-	int target[MAX];
-	char guess[MAX];
-	int red = 0, count =0,white=0;
-
-	int onePlace[10]   = {0,1,2,3,4,5,6,7,8,9};
-	int twoPlace[10]   = {0,1,2,3,4,5,6,7,8,9};
-	int threePlace[10] = {0,1,2,3,4,5,6,7,8,9};
-	int fourPlace[10]  = {0,1,2,3,4,5,6,7,8,9};
-
-	printf("Target is: ");
-	
-	for (int i = 0; i < 4; i++)
-	{
-		target[i] = rand() % 10;
-		printf("%d",target[i]);
-	}
-	printf("\n");
-
-	while(red != 4)
-	{
-		compInput(guess,red,white,&count,onePlace,twoPlace,threePlace,fourPlace);
-		red = compare(red, white,target,guess);
-		count +=1;
-	}
-	printf("You've won in %d guesses.\n",count);
-
-	return(0);
-}
-
-
-void compInput(char* guess,int red, int white, int* count,int* onePlace,int* twoPlace,int* threePlace,int* fourPlace)
-{
-	printf("%c %d %d %p %d %d %d %d",guess[0],red, white, (void*)count, onePlace[0], twoPlace[0], threePlace[0], fourPlace[0]);
-	return;
-}
 
 int game()
 {
@@ -65,10 +26,9 @@ int game()
 	int red = 0, count =0,white=0;
 
 	printf("Target is: ");
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < FOUR; i++)
 	{
 		target[i] = rand() % 10;
-		//printf("%d",target[i]);
 	}
 	printf("\n");
 
@@ -83,7 +43,7 @@ int game()
 	printf("%d\n",target[3]);
 
 
-	while(red != 4)
+	while(red != FOUR)
 	{
 		userInput(guess);
 		red = compare(red, white,target,guess);
@@ -94,17 +54,17 @@ int game()
 	return(0);
 }
 
+
+
 int compare(int red, int white,int target[], char guess[])
 {
 	red = 0;
 	white = 0;
-	int skip[4] = {0};
-	int whiteSkip[4] = {0};
-
-
+	int skip[FOUR] = {0};
+	int whiteSkip[FOUR] = {0};
 
 	// Red Loop, sets indexes to skip
-	for(int i = 0; i < 4;i++)
+	for(int i = 0; i < FOUR;i++)
 	{
 		if(target[i] == guess[i])
 		{
@@ -117,16 +77,14 @@ int compare(int red, int white,int target[], char guess[])
 	printf("%d",skip[1]);
 	printf("%d",skip[2]);
 	printf("%d\n",skip[3]);
-	for(int p = 0; p <4; p++)
+	for(int p = 0; p <FOUR; p++)
 	{
-		//printf("\n\np is: %d\n",p);
 		if(skip[p])
 		{
-			//whiteSkip[p] = 1;
 			continue;
 		}
 
-		for(int x = 0;x<4; x++)
+		for(int x = 0;x<FOUR; x++)
 		{
 			if(whiteSkip[x])
 			{
@@ -134,7 +92,7 @@ int compare(int red, int white,int target[], char guess[])
 			}
 			else if(guess[p] == target[x])
 			{
-				//printf("White found\n");
+				//White found
 				white +=1;
 				whiteSkip[x] = 1;
 				skip[p] = 1;
@@ -155,13 +113,13 @@ void userInput(char* user)
 	int valid = 0, check = 0;
 	char c;
 
-	while(!valid)
+	while(!valid) // Loop until valid input is entered
 	{
 		check = 0;
 		printf("Guess a number: ");
 		fgets(user,MAX,stdin);
 		
-		if(user[4]!= '\n') // Checking for four characters.
+		if(user[FOUR]!= '\n') // Checking for four characters.
 		{
 			printf("Please enter FOUR numbers.\n");
 			if(strlen(user) < 5)
@@ -177,23 +135,21 @@ void userInput(char* user)
 		}
 		else // Entered four characters plus a \n.
 		{
-			for(int i = 0; i < 4; i++) // iterate through those four characters
+			for(int i = 0; i < FOUR; i++) // iterate through those four characters
 			{
 				if( (user[i] > '9') || (user[i] < '0') ) // Check for non number character
 				{
-					check = 1;
+					check = 1; // Found a non-number character
 					break;
 				}
 			}
 			if(check) //Non number character entered.
 			{
-				printf("letter encountered.\n");
-				while ( (c=getchar()) != '\n' && c != EOF ) // Loop to clear out the stdin buffer.
-		    		/*Do nothing*/;
+				printf("Please enter four NUMBERS.\n");
 				continue;
 			}
 			valid = 1; // Validation complete with only 4 numbers.
-			for(int i = 0;i < 4; i++)
+			for(int i = 0;i < FOUR; i++)
 			{
 				user[i] = user[i] - '0';
 			}
